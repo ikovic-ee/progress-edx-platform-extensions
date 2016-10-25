22756451c066c8e853bd7367e47178e1e48f58a8
Ivan Kovic <kovic.ivan@gmail.com>
2016-10-25 13:18:44 +0200

    Imported the missing signal vol2

--- progress/signals.py
+++ progress/signals.py
@@ -14,12 +14,7 @@ from opaque_keys.edx.locator import BlockUsageLocator
 from student.roles import get_aggregate_exclusion_user_ids
 from util.signals import course_deleted
 
-from edx_notifications.lib.publisher import (
-    publish_notification_to_user,
-    get_notification_type
-)
-from edx_notifications.data import NotificationMessage
-from course_metadata.utils import is_progress_detached_vertical
+#from course_metadata.utils import is_progress_detached_vertical
 
 from progress.models import StudentProgress, StudentProgressHistory, CourseModuleCompletion
 
@@ -39,8 +34,9 @@ def is_valid_progress_module(content_id):
         detached_categories = getattr(settings, 'PROGRESS_DETACHED_CATEGORIES', [])
         usage_id = BlockUsageLocator.from_string(content_id)
         module = modulestore().get_item(usage_id)
-        if module and module.parent and module.parent.category == "vertical" and \
-                module.category not in detached_categories and not is_progress_detached_vertical(module.parent):
+        if module and module.parent and module.parent.category == "vertical" \
+                and module.category not in detached_categories:
+            # module.category not in detached_categories and not is_progress_detached_vertical(module.parent):
             return True
         else:
             return False
@@ -114,53 +110,7 @@ def handle_progress_post_save_signal(sender, instance, **kwargs):  # pylint: dis
     """
     Handle the pre-save ORM event on CourseModuleCompletions
     """
-
-    if settings.FEATURES['ENABLE_NOTIFICATIONS']:
-        # If notifications feature is enabled, then we need to get the user's
-        # rank before the save is made, so that we can compare it to
-        # after the save and see if the position changes
-        leaderboard_rank = StudentProgress.get_user_position(
-            instance.course_id,
-            instance.user.id,
-            get_aggregate_exclusion_user_ids(instance.course_id)
-        )['position']
-
-        # logic for Notification trigger is when a user enters into the Leaderboard
-        leaderboard_size = getattr(settings, 'LEADERBOARD_SIZE', 3)
-        presave_leaderboard_rank = sys.maxint
-        if instance.presave_leaderboard_rank:
-            presave_leaderboard_rank = instance.presave_leaderboard_rank
-        if leaderboard_rank <= leaderboard_size and presave_leaderboard_rank > leaderboard_size:
-            try:
-                notification_msg = NotificationMessage(
-                    msg_type=get_notification_type(u'open-edx.lms.leaderboard.progress.rank-changed'),
-                    namespace=unicode(instance.course_id),
-                    payload={
-                        '_schema_version': '1',
-                        'rank': leaderboard_rank,
-                        'leaderboard_name': 'Progress',
-                    }
-                )
-
-                #
-                # add in all the context parameters we'll need to
-                # generate a URL back to the website that will
-                # present the new course announcement
-                #
-                # IMPORTANT: This can be changed to msg.add_click_link() if we
-                # have a particular URL that we wish to use. In the initial use case,
-                # we need to make the link point to a different front end website
-                # so we need to resolve these links at dispatch time
-                #
-                notification_msg.add_click_link_params({
-                    'course_id': unicode(instance.course_id),
-                })
-
-                publish_notification_to_user(int(instance.user.id), notification_msg)
-            except Exception, ex:  # pylint: disable=broad-except
-                # Notifications are never critical, so we don't want to disrupt any
-                # other logic processing. So log and continue.
-                log.exception(ex)
+    pass
 
 
 @receiver(course_deleted)
